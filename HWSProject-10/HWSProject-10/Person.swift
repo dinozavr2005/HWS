b86f7d108999f46c6c04c96835f7822b4b66db41
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2022-01-18 16:02:28 +0300

    project10

---
+++ HWSProject-10/HWSProject-10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  HWSProject-10
+//
+//  Created by Владимир on 18.01.2022.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ HWSProject-10/HWSProject-10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  HWSProject-10
+//
+//  Created by Владимир on 17.01.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
