5666fb2d13bba19fa651eddbc1b03a5e994e6bc0
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2022-02-02 19:26:04 +0300

    SwiftUI Proj1

---
+++ SwiftUI/HWSProject1/HWSProject1/HWSProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  HWSProject1App.swift
+//  HWSProject1
+//
+//  Created by Владимир on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HWSProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
